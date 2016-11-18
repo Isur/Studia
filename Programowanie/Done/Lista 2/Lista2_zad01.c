55753b3c538880b790a809c2e437e7d096079fce
Isur <arturbed96@gmail.com>
2016-11-18 22:54:09 +0100

    Zadanie 12 Dodanie. Komentarz do zad 01.

--- Programowanie/Done/Lista 2/Lista2_zad01.c
+++ Programowanie/Done/Lista 2/Lista2_zad01.c
@@ -1,15 +1,22 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
-
+/*
+Napisz program, który oblicza ilość wystąpień poszczególnych znaków w podanym przez użytkownika ciągu. Program rozróżnia wielkie i małe litery oraz akceptuje
+wszystkie symbole, które można wprowadzić z klawiatury. Np. poprawnie obliczy ilość
+wystąpień wszystkich znaków z ciągu: Ala123 @#$%!.()[]-+?>’"kot
+Na ekranie powinna pojawić się lista tych symboli, które pojawiły się co najmniej jeden
+raz wraz z liczbą ich wystąpień
+*/
 main()
 {
-	char chars[1024];
-	int count[1024][2] = {0,0};
-	int i = 0, j = 0, quantity = 0, change = 0;
+	char chars[1024];                               // Podane znaki
+	int count[1024][2] = {0,0};                     // znak - ilosc 
+	int i = 0, j = 0, quantity = 0, change = 0;     // zmienne pomocnicze
 	printf("Wpisz ciag znakow: ");
 	fgets(chars, 1024, stdin);
 
+	// Sprawdzanie czy znaki sie powtarzaja, 
 	for (i = 0; i < strlen(chars); i++)
 	{
 		if (i == 0)
@@ -36,6 +43,7 @@ main()
 			}
 		}
 	}
+	// Wypisywanie znakow i ich ilosci
 	for (i = 0; i<quantity; i++)
 	{
 		printf("%c - %d\n", count[i][0], count[i][1]);
