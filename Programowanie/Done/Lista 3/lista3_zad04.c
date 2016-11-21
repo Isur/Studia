9e18f6c5881a3a2692487e5f4e2616abcf63e479
Artur <arturbed96@gmail.com>
2016-11-21 09:50:15 +0100

    Dodano zadanie

---
+++ Programowanie/Done/Lista 3/lista3_zad04.c
@@ -0,0 +1,9 @@
+/* SprawdŸ, ile bajtów pamiêci zajmuje zmienna wskaŸnikowa. */
+#include<stdio.h>
+main()
+{
+    int *p;
+    printf("Wskaznik zajmuje %d bajty \n", sizeof(p));
+    system("PAUSE");
+    return 0;
+}
