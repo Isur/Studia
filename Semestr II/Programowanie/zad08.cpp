dfc1c557d8fb79c4cdc22e20139519875d34c1d9
Artur <arturbed96@gmail.com>
2017-03-02 01:28:58 +0100

    Zadanie 08

---
+++ Semestr II/Programowanie/zad08.cpp
@@ -0,0 +1,77 @@
+/*
+Zadanie:
+Wybierz cztery operatory, które bêdziesz prze³adowywa³ (dwa jednoargumentowe oraz dwa dwuargumentowe). 
+Nastêpnie napisz funkcje prze³adowuj¹ce.
+Ka¿dy typ operatora powinien mieæ utworzon¹ funkcjê w postaci funkcji globalnej oraz metody klasy.
+*/
+#include<iostream>
+using namespace std;
+
+class Number
+{
+public:
+	int x;
+
+	Number()
+	{
+		x = 1;
+	}
+	Number(int x)
+	{
+		this->x = x;
+	}
+	friend Number operator+(const Number& x, const Number& y);
+	friend Number operator%(const Number& x, const Number& y);
+	friend Number operator!(const Number& x);
+	friend Number operator~(const Number& x);
+
+	void show()
+	{
+		cout << this->x;
+	}
+};
+Number operator+(const Number& x, const Number& y)
+{
+	return Number(x.x + y.x);
+}
+Number operator%(const Number& x, const Number& y)
+{
+	return Number(x.x % y.x);
+}
+Number operator!(const Number& x)
+{
+	return Number(-x.x);
+}
+Number operator~(const Number& x)
+{
+	int g = ~x.x;
+	return Number(g);
+}
+
+
+int main()
+{
+	Number x(7);
+	Number y(5);
+	Number r(0);
+	cout << "Obiekt 1: ";
+	x.show();
+	cout << endl;
+	cout << "Obiekt 2: ";
+	y.show();
+	cout << "\nOperator 2-argumentowy + \n";
+	r = x + y;
+	r.show();
+	cout << "\nOperator 2-argumentowy % \n";
+	r = x % y;	
+	r.show();
+	cout << "\nOperator 1-argumentowy ! (na obiekcie 1) \n";
+	r = !x;
+	r.show();
+	cout << "\nOperator 1-argumentowy ~ (na obiekcie 1)\n";
+ 	r = ~x;
+	r.show();
+
+	cout << endl;
+	return 0;
+}
