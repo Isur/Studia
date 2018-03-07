7362bda7bfea01a0094baafad6f141b3a489b6e4
Artur <arturbed96@gmail.com>
2018-03-07 13:40:19 +0100

    lab_03

---
+++ Semestr IV/lab_02/lab_02/Form1.Designer.cs
@@ -0,0 +1,228 @@
+﻿namespace lab_02
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.groupBoxUser = new System.Windows.Forms.GroupBox();
+            this.buttonEdit = new System.Windows.Forms.Button();
+            this.buttonAdd = new System.Windows.Forms.Button();
+            this.numericUpDownAge = new System.Windows.Forms.NumericUpDown();
+            this.labelAge = new System.Windows.Forms.Label();
+            this.textBoxSurname = new System.Windows.Forms.TextBox();
+            this.labelNazwisko = new System.Windows.Forms.Label();
+            this.textBoxName = new System.Windows.Forms.TextBox();
+            this.labelName = new System.Windows.Forms.Label();
+            this.listBoxUsers = new System.Windows.Forms.ListBox();
+            this.errorProvider1 = new System.Windows.Forms.ErrorProvider(this.components);
+            this.buttonSave = new System.Windows.Forms.Button();
+            this.buttonLoad = new System.Windows.Forms.Button();
+            this.groupBoxUser.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownAge)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBoxUser
+            // 
+            this.groupBoxUser.Controls.Add(this.buttonLoad);
+            this.groupBoxUser.Controls.Add(this.buttonSave);
+            this.groupBoxUser.Controls.Add(this.buttonEdit);
+            this.groupBoxUser.Controls.Add(this.buttonAdd);
+            this.groupBoxUser.Controls.Add(this.numericUpDownAge);
+            this.groupBoxUser.Controls.Add(this.labelAge);
+            this.groupBoxUser.Controls.Add(this.textBoxSurname);
+            this.groupBoxUser.Controls.Add(this.labelNazwisko);
+            this.groupBoxUser.Controls.Add(this.textBoxName);
+            this.groupBoxUser.Controls.Add(this.labelName);
+            this.groupBoxUser.Location = new System.Drawing.Point(12, 12);
+            this.groupBoxUser.Name = "groupBoxUser";
+            this.groupBoxUser.Size = new System.Drawing.Size(247, 178);
+            this.groupBoxUser.TabIndex = 0;
+            this.groupBoxUser.TabStop = false;
+            this.groupBoxUser.Text = "Użytkownik";
+            // 
+            // buttonEdit
+            // 
+            this.buttonEdit.Location = new System.Drawing.Point(99, 101);
+            this.buttonEdit.Name = "buttonEdit";
+            this.buttonEdit.Size = new System.Drawing.Size(75, 23);
+            this.buttonEdit.TabIndex = 5;
+            this.buttonEdit.Text = "Edytuj";
+            this.buttonEdit.UseVisualStyleBackColor = true;
+            this.buttonEdit.Click += new System.EventHandler(this.buttonEdit_Click);
+            // 
+            // buttonAdd
+            // 
+            this.buttonAdd.Location = new System.Drawing.Point(11, 102);
+            this.buttonAdd.Name = "buttonAdd";
+            this.buttonAdd.Size = new System.Drawing.Size(81, 23);
+            this.buttonAdd.TabIndex = 3;
+            this.buttonAdd.Text = "Dodaj";
+            this.buttonAdd.UseVisualStyleBackColor = true;
+            this.buttonAdd.Click += new System.EventHandler(this.buttonAdd_Click);
+            // 
+            // numericUpDownAge
+            // 
+            this.numericUpDownAge.Location = new System.Drawing.Point(131, 73);
+            this.numericUpDownAge.Maximum = new decimal(new int[] {
+            70,
+            0,
+            0,
+            0});
+            this.numericUpDownAge.Minimum = new decimal(new int[] {
+            18,
+            0,
+            0,
+            0});
+            this.numericUpDownAge.Name = "numericUpDownAge";
+            this.numericUpDownAge.Size = new System.Drawing.Size(36, 20);
+            this.numericUpDownAge.TabIndex = 2;
+            this.numericUpDownAge.Value = new decimal(new int[] {
+            25,
+            0,
+            0,
+            0});
+            this.numericUpDownAge.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.numericUpDownAge_KeyPress);
+            // 
+            // labelAge
+            // 
+            this.labelAge.AutoSize = true;
+            this.labelAge.Location = new System.Drawing.Point(8, 75);
+            this.labelAge.Name = "labelAge";
+            this.labelAge.Size = new System.Drawing.Size(32, 13);
+            this.labelAge.TabIndex = 4;
+            this.labelAge.Text = "Wiek";
+            // 
+            // textBoxSurname
+            // 
+            this.textBoxSurname.Location = new System.Drawing.Point(67, 46);
+            this.textBoxSurname.Name = "textBoxSurname";
+            this.textBoxSurname.Size = new System.Drawing.Size(100, 20);
+            this.textBoxSurname.TabIndex = 1;
+            this.textBoxSurname.TextChanged += new System.EventHandler(this.textBoxName_TextChanged);
+            this.textBoxSurname.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBoxSurname_KeyPress);
+            // 
+            // labelNazwisko
+            // 
+            this.labelNazwisko.AutoSize = true;
+            this.labelNazwisko.Location = new System.Drawing.Point(8, 49);
+            this.labelNazwisko.Name = "labelNazwisko";
+            this.labelNazwisko.Size = new System.Drawing.Size(53, 13);
+            this.labelNazwisko.TabIndex = 2;
+            this.labelNazwisko.Text = "Nazwisko";
+            // 
+            // textBoxName
+            // 
+            this.textBoxName.Location = new System.Drawing.Point(67, 20);
+            this.textBoxName.Name = "textBoxName";
+            this.textBoxName.Size = new System.Drawing.Size(100, 20);
+            this.textBoxName.TabIndex = 0;
+            this.textBoxName.TextChanged += new System.EventHandler(this.textBoxName_TextChanged);
+            this.textBoxName.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBoxName_KeyPress);
+            // 
+            // labelName
+            // 
+            this.labelName.AutoSize = true;
+            this.labelName.Location = new System.Drawing.Point(8, 23);
+            this.labelName.Name = "labelName";
+            this.labelName.Size = new System.Drawing.Size(26, 13);
+            this.labelName.TabIndex = 0;
+            this.labelName.Text = "Imię";
+            // 
+            // listBoxUsers
+            // 
+            this.listBoxUsers.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.listBoxUsers.FormattingEnabled = true;
+            this.listBoxUsers.Location = new System.Drawing.Point(266, 22);
+            this.listBoxUsers.Name = "listBoxUsers";
+            this.listBoxUsers.Size = new System.Drawing.Size(236, 160);
+            this.listBoxUsers.TabIndex = 1;
+            this.listBoxUsers.SelectedIndexChanged += new System.EventHandler(this.listBoxUsers_SelectedIndexChanged);
+            // 
+            // errorProvider1
+            // 
+            this.errorProvider1.ContainerControl = this;
+            // 
+            // buttonSave
+            // 
+            this.buttonSave.Location = new System.Drawing.Point(11, 146);
+            this.buttonSave.Name = "buttonSave";
+            this.buttonSave.Size = new System.Drawing.Size(75, 23);
+            this.buttonSave.TabIndex = 6;
+            this.buttonSave.Text = "Zapisz";
+            this.buttonSave.UseVisualStyleBackColor = true;
+            this.buttonSave.Click += new System.EventHandler(this.buttonSave_Click);
+            // 
+            // buttonLoad
+            // 
+            this.buttonLoad.Location = new System.Drawing.Point(99, 146);
+            this.buttonLoad.Name = "buttonLoad";
+            this.buttonLoad.Size = new System.Drawing.Size(75, 23);
+            this.buttonLoad.TabIndex = 7;
+            this.buttonLoad.Text = "Wczytaj";
+            this.buttonLoad.UseVisualStyleBackColor = true;
+            this.buttonLoad.Click += new System.EventHandler(this.buttonLoad_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(514, 196);
+            this.Controls.Add(this.listBoxUsers);
+            this.Controls.Add(this.groupBoxUser);
+            this.Name = "Form1";
+            this.Text = "Lab_02";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBoxUser.ResumeLayout(false);
+            this.groupBoxUser.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownAge)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBoxUser;
+        private System.Windows.Forms.Button buttonAdd;
+        private System.Windows.Forms.NumericUpDown numericUpDownAge;
+        private System.Windows.Forms.Label labelAge;
+        private System.Windows.Forms.TextBox textBoxSurname;
+        private System.Windows.Forms.Label labelNazwisko;
+        private System.Windows.Forms.TextBox textBoxName;
+        private System.Windows.Forms.Label labelName;
+        private System.Windows.Forms.ListBox listBoxUsers;
+        private System.Windows.Forms.ErrorProvider errorProvider1;
+        private System.Windows.Forms.Button buttonEdit;
+        private System.Windows.Forms.Button buttonLoad;
+        private System.Windows.Forms.Button buttonSave;
+    }
+}
+
---
+++ Semestr IV/lab_02/lab_02/Form1.cs
@@ -0,0 +1,188 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace lab_02
+{
+    public partial class Form1 : Form
+    {
+        string user = "";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        #region MyMethods
+        private bool IsFilled(TextBox tb)
+        {
+            if(tb.Text == "")
+            {
+                errorProvider1.SetError(tb, "Wypełnij pole");
+                return false;
+            }
+            errorProvider1.SetError(tb, "");
+            return true;
+
+        }
+
+        private void ClearForm()
+        {
+            textBoxName.Clear();
+            textBoxSurname.Clear();
+            numericUpDownAge.Value = 25;
+            user = "";
+        }
+
+        private bool AddUser(string user)
+        {
+            if (listBoxUsers.Items.Contains(user))
+                return false;
+            listBoxUsers.Items.Add(user);
+            return true;
+        }
+
+        private bool EditUser(string user)
+        {
+           if (listBoxUsers.Items.Contains(user))
+                return false;
+            if (MessageBox.Show("Chcesz zmienic dane na: ", user, MessageBoxButtons.YesNo) == DialogResult.Yes)
+            {
+                listBoxUsers.Items[listBoxUsers.SelectedIndex] = user;
+                return false;
+            }
+           return true;
+        }
+        private bool IsLastLetter(string text)
+        {
+                return (char.IsLetter(text[text.Length - 1]));
+        }
+
+        #endregion
+
+        #region EventsMethods
+        private void buttonAdd_Click(object sender, EventArgs e)
+        {
+            if (IsFilled(textBoxName) & IsFilled(textBoxSurname))
+            {
+                user += textBoxName.Text.Trim() + " " + textBoxSurname.Text.Trim() + " " + numericUpDownAge.Value;
+                if (AddUser(user))
+                {
+                    ClearForm();
+                    textBoxName.Focus();
+                }
+            }
+
+        }
+
+        private void textBoxName_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if(e.KeyChar == 13)
+            {
+                textBoxSurname.Focus();
+            }
+        }
+
+        private void textBoxSurname_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == 13)
+            {
+                numericUpDownAge.Focus();
+            }
+        }
+
+        private void numericUpDownAge_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == 13)
+            {
+                buttonAdd_Click(sender, e);
+            }
+        }
+        private void textBoxName_TextChanged(object sender, EventArgs e)
+        {
+            TextBox tb = sender as TextBox;
+            if (tb.Text.Length > 0)
+            {
+                if (!IsLastLetter(tb.Text))
+                {
+                    MessageBox.Show("Wpisałeś nie literę.");
+                    tb.Text = tb.Text.Remove(tb.Text.Length - 1);
+                    tb.Focus();
+                    tb.SelectionStart = tb.Text.Length;
+                }
+            }
+        }
+        private void listBoxUsers_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if(listBoxUsers.SelectedIndex == -1)
+            {
+                textBoxName.Text = "";
+                textBoxSurname.Text = "";
+                numericUpDownAge.Value = 25;
+            }else
+            {
+                decimal age;
+                string[] words = listBoxUsers.SelectedItem.ToString().Split(' ');
+                textBoxName.Text = words[0];
+                textBoxSurname.Text = words[1];
+               if( decimal.TryParse(words[2], out age))
+                {
+                    numericUpDownAge.Value = age;
+                }
+            }
+        }
+        private void buttonEdit_Click(object sender, EventArgs e)
+        {
+            if (IsFilled(textBoxName) & IsFilled(textBoxSurname))
+            {
+                user += textBoxName.Text.Trim() + " " + textBoxSurname.Text.Trim() + " " + numericUpDownAge.Value;
+                if (EditUser(user))
+                {
+                    ClearForm();
+                    textBoxName.Focus();
+                }
+            }
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            if (File.Exists("save.txt"))
+            {
+                string[] lines = File.ReadAllLines("save.txt");
+                for (int i = 0; i < lines.Length; i++)
+                {
+                    listBoxUsers.Items.Add(lines[i]);
+                }
+            }
+        }
+
+        private void buttonSave_Click(object sender, EventArgs e)
+        {
+            string[] lines = new string[listBoxUsers.Items.Count];
+            for (int i = 0; i < listBoxUsers.Items.Count; i++)
+            {
+                lines[i] = listBoxUsers.Items[i].ToString();
+            }
+            File.WriteAllLines("save.txt",lines);
+        }
+
+        #endregion
+
+        private void buttonLoad_Click(object sender, EventArgs e)
+        {
+            if (File.Exists("save.txt"))
+            {
+                string[] lines = File.ReadAllLines("save.txt");
+                for (int i = 0; i < lines.Length; i++)
+                {
+                    listBoxUsers.Items.Add(lines[i]);
+                }
+            }
+        }
+    }
+}
